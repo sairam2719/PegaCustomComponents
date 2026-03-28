ab745427fd5a4a16a599a220023a09387f54f237
sairam2719 <43761755+sairam2719@users.noreply.github.com>
2026-03-28 19:15:50 +0530

    Add files via upload

---
+++ components/src/components/Sl_DXExtensions_PegaExtensionsActionableButton/demo.stories.tsx
@@ -0,0 +1,68 @@
+import type { StoryObj } from '@storybook/react';
+
+import PegaExtensionsActionableButton from './index';
+
+export default {
+  title: 'Fields/Actionable button',
+  argTypes: {
+    getPConnect: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  component: PegaExtensionsActionableButton
+};
+
+const setPCore = () => {
+  (window as any).PCore = {
+    getComponentsRegistry: () => {
+      return {
+        getLazyComponent: (f: string) => f
+      };
+    },
+    getEnvironmentInfo: () => {
+      return {
+        getTimeZone: () => 'local'
+      };
+    }
+  };
+};
+
+type Story = StoryObj<typeof PegaExtensionsActionableButton>;
+
+export const Default: Story = {
+  render: args => {
+    setPCore();
+    const props = {
+      ...args,
+      getPConnect: () => {
+        return {
+          getStateProps: () => {
+            return {
+              value: 'C-123'
+            };
+          },
+          getActionsApi: () => {
+            return {
+              openLocalAction: {
+                bind: () => {
+                  return (name: string, options: any) => {
+                    // eslint-disable-next-line no-alert
+                    alert(`Launch local action ${name} for ${options.caseID}`);
+                  };
+                }
+              }
+            };
+          }
+        };
+      }
+    };
+    return <PegaExtensionsActionableButton {...props} />;
+  },
+  args: {
+    label: 'Launch',
+    localAction: 'pyEditDetails',
+    value: 'Work-Case C-123'
+  }
+};
